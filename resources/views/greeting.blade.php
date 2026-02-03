11cf0f45ca562b768d77fd931023d46fe4be1990
syl566 <sylvie.bal@le-campus-numerique.fr>
2026-02-03 10:18:23 +0100

    Partie 2 - greeting.balde.php

---
+++ resources/views/greeting.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ShopLaravel</title>
+</head>
+
+<body>
+    <h1>ShopLaravel, {{ $title }}</h1>
+    <p>Bienvenue sur Shoplaravel</p>
+    <div>
+        <!-- It is not the man who has too little, but the man who craves more, that is poor. - Seneca -->
+    </div>
+
+</body>
+
+</html>
