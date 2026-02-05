034d0c2879a651474f68bd5c79a2a80ba42e1c7c
syl566 <sylvie.bal@le-campus-numerique.fr>
2026-02-05 11:39:04 +0100

    Partie 3 - create_products_table.php

--- database/migrations/2026_02_05_101531_create_products_table.php
+++ database/migrations/2026_02_05_101531_create_products_table.php
@@ -11,16 +11,17 @@ return new class extends Migration
      */
     public function up(): void
     {
-       Schema::create('category', function (Blueprint $table) {
-            $table->id();                    // Clé primaire auto-incrémentée
-            $table->string('name');          // VARCHAR(255)
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();   // Clé primaire auto-incrémentée
+            $table->foreignId('category_id')->constrained('categories');
+            $table->string('name');          // VARCHAR(255)                      
             $table->string('slug')->unique();
             $table->text('description')->nullable(); // TEXT
             $table->decimal('price', 8, 2);  // DECIMAL(8,2) pour les prix
             $table->integer('stock');        // INTEGER
             $table->boolean('active')->default(true);
             $table->timestamps();            // créé_à et mis_à_jour
-    });
+        });
     }
 
     /**
