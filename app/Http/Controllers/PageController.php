050850b06bfe4b21b46272d5114314b1f22e8ec8
syl566 <sylvie.bal@le-campus-numerique.fr>
2026-02-05 09:40:30 +0100

    Partie 2 -PageController

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,21 +6,54 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
+    public function index()
+    {
+        $articles = [
+            [
+                'id' => 1,
+                'name' => 'Article 1',
+                'price' => 20,
+                'stock' => 0
+            ],
+            [
+                'id' => 2,
+                'name' => 'Article 2',
+                'price' => 30,
+                'stock'=> 10
+            ],
+            [
+                'id' => 3,
+                'name' => 'Article 3',
+                'price' => 40,
+                'stock' => 15
+            ],
+            [
+                'id' => 4,
+                'name' => 'Article 4',
+                'price' => 50,
+                'stock' => 20
+            ],
+            [
+                'id' => 5,
+                'name' => 'Article 5',
+                'price' => 60,
+                'stock'=> 2
+            ]
+        ];
+        return view('index', compact('articles'));
+    }
 
-    public function home() {
-     $product = [
-        'name' => 'T-shirt Laravel',
-        'price' => 29.99,
-        'description' => 'Un super t-shirt',
-        'stock' => 0
-
-    ];
-     return view('greeting', compact('product'));
-    
+    public function home()
+    {
+        return view('home');
     }
 
     public function about()
     {
-        return 'A propos de nous';
+        return view('about');
+    }
+    public function contact()
+    {
+        return view('contact');
     }
 }
