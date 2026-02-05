3929105559cb24445241f17152d703205aadd1c8
syl566 <sylvie.bal@le-campus-numerique.fr>
2026-02-05 10:30:24 +0100

    Partie 3 - CREATE_products_table.php

---
+++ database/migrations/2026_02_05_084432_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+       Schema::create('category', function (Blueprint $table) {
+            $table->id();                    // Clé primaire auto-incrémentée
+            $table->string('name');          // VARCHAR(255)
+            $table->string('slug')->unique();
+            $table->text('description')->nullable(); // TEXT
+            $table->decimal('price', 8, 2);  // DECIMAL(8,2) pour les prix
+            $table->integer('stock');        // INTEGER
+            $table->boolean('active')->default(true);
+            $table->timestamps();            // créé_à et mis_à_jour
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
